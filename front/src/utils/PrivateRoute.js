858820c6d9cce9c71785c8bffbc5b06a3487fe5b
Samlogy <senanisammy@gmail.com>
2021-09-12 10:41:18 +0200

    add: private route

--- front/src/utils/PrivateRoute.js
+++ front/src/utils/PrivateRoute.js
@@ -1,10 +1,18 @@
 import React from 'react'
 import { Route, Redirect } from 'react-router-dom'
+import { useSelector } from "react-redux"
 
-const PrivateRoute = ({
-    component: Component, isLogged, userLevel, ...rest 
-    }) => (
-        <Route {...rest} component={(props) => isLogged ? <Component {...props} /> : <Redirect to="/login" />
-    } />
-)
-export default PrivateRoute 
+const PrivateRoute = ({ component: Component, ...rest }) => {
+
+    const authStore = useSelector(state => state.auth);
+    const isLogged = authStore.logged;
+
+    return(
+        <Route {...rest} 
+            component={(props) => isLogged ? 
+                <Component {...props} /> : <Redirect to="/login" />} />
+    )
+}
+
+export default PrivateRoute;
+
