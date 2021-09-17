10f7c29bfe57987a7a751149ffd3a9b6c7377a29
Samlogy <senanisammy@gmail.com>
2021-09-17 16:36:30 +0200

    add: (front) login page full responsive

--- front/src/Pages/Auth/Login.jsx
+++ front/src/Pages/Auth/Login.jsx
@@ -1,7 +1,7 @@
 import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { useHistory } from "react-router-dom";
-import { Heading, Input, Button, FormControl, Stack, Box, InputRightElement, InputGroup, FormLabel,
+import { Heading, Input, Button, FormControl, Stack, Box, InputRightElement, InputGroup, FormLabel, Container,
   useToast, useColorModeValue } from "@chakra-ui/react";
   
 import Layout from "../Layout.jsx"
@@ -49,42 +49,44 @@ const Login = () => {
     };
     
   return    <Layout>
-              <Stack spacing={8} mx={'auto'} maxW={'lg'} py={12} px={6} minH="100vh" display="flex" justifyContent="center" alignItems="center">
-                <Stack align={'center'}>
-                  <Heading fontSize={'4xl'}> Sign In </Heading>
-                </Stack>
-       
-                <Box rounded={'lg'} bg={useColorModeValue('white', 'gray.700')} boxShadow={'lg'} p={8} w="500px">
-                  <Stack spacing={4}>
-                    <FormControl id="email">
-                      <FormLabel> Email </FormLabel>
-                      <Input type="email" placeholder="Email" value={auth.email}
-                                  onChange={(e) => setAuth({...auth, email: e.target.value})} />
-                    </FormControl>
-
-                    <FormControl id="password">
-                    <FormLabel> Password </FormLabel>
-                      <InputGroup>
-                        <Input type={showPassword ? "text" : "password"} placeholder="Password" value={auth.password} 
-                                    onChange={(e) => setAuth({...auth, password: e.target.value})} />
-                        <InputRightElement width="4.5rem">
-
-                          <Button h="1.75rem" size="sm" onClick={handleShowClick}>
-                            {showPassword ? "Hide" : "Show"}
-                          </Button>
-                        </InputRightElement>
-                      </InputGroup>
-                    </FormControl>
-                    
-
-                    <Stack spacing={10}>
-                      <Button bg={'blue.400'} borderRadius="md" color={'white'} _hover={{ bg: 'blue.500', }} onClick={() => onLogin()}>
-                        Sign In
-                      </Button>
-                    </Stack>
+              <Container maxW="500px" bg="white" py="39px" px={["16px","","","40px"]} m="0 auto" borderRadius="4px">
+                <Stack spacing={8} mx={'auto'} p="12px 6px" minH="100vh" justifyContent="center" alignItems="center">
+                  <Stack align={'center'}>
+                    <Heading fontSize={'4xl'}> Sign In </Heading>
                   </Stack>
-                </Box>
-              </Stack>
+        
+                  <Box rounded={'lg'} bg={useColorModeValue('white', 'gray.700')} boxShadow={'lg'} p={8} w="100%">
+                    <Stack spacing={4}>
+                      <FormControl id="email">
+                        <FormLabel> Email </FormLabel>
+                        <Input type="email" placeholder="Email" value={auth.email}
+                                    onChange={(e) => setAuth({...auth, email: e.target.value})} />
+                      </FormControl>
+
+                      <FormControl id="password">
+                      <FormLabel> Password </FormLabel>
+                        <InputGroup>
+                          <Input type={showPassword ? "text" : "password"} placeholder="Password" value={auth.password} 
+                                      onChange={(e) => setAuth({...auth, password: e.target.value})} />
+                          <InputRightElement width="4.5rem">
+
+                            <Button h="1.75rem" size="sm" onClick={handleShowClick}>
+                              {showPassword ? "Hide" : "Show"}
+                            </Button>
+                          </InputRightElement>
+                        </InputGroup>
+                      </FormControl>
+                      
+
+                      <Stack spacing={10}>
+                        <Button bg={'blue.400'} borderRadius="md" color={'white'} _hover={{ bg: 'blue.500', }} onClick={() => onLogin()}>
+                          Sign In
+                        </Button>
+                      </Stack>
+                    </Stack>
+                  </Box>
+                </Stack>
+              </Container>
             </Layout>
 };
 
