deff39ce0509715a7268cf73432be9b88bdb11d4
Samlogy <senanisammy@gmail.com>
2021-09-18 15:55:36 +0200

    refactor: (front) all api calls --> notifications (load - delete - hide)

--- front/src/api/index.js
+++ front/src/api/index.js
@@ -43,9 +43,72 @@ export const logout = async () => {
     }
 };
 
+/* Notifications */
+export const load_notifications = async (userId) => {
+    try {
+        const res = await request("get", `/notifications/${userId}`, {});
+        if (res.success) {
+            const { data, success, message } = res;
+            return {
+                success,
+                data,
+                message
+            }
+        }
+
+    } catch (err) {
+        console.log('Operation Error occured --> : ', err);
+        return {
+            success: false,
+            error: err.message
+        }
+    }
+};
+
+export const delete_notification = async (notificationId) => {
+    try {
+        const res = await request("delete", `/notifications/delete/${notificationId}`, {});
+        if (res.success) {
+            const { data, success, message } = res;
+            return {
+                success,
+                data,
+                message
+            }
+        }
+
+    } catch (err) {
+        console.log('Operation Error occured --> : ', err);
+        return {
+            success: false,
+            error: err.message
+        }
+    }
+};
+
+export const hide_notification = async(notificationId) => {
+    try {
+        const res = await request("put", `/notifications/hide/${notificationId}/${'hidden'}`, {});
+        if (res.success) {
+            const { data, success, message } = res;
+            return {
+                success,
+                data,
+                message
+            }
+        }
+
+    } catch (err) {
+        console.log('Operation Error occured --> : ', err);
+        return {
+            success: false,
+            error: err.message
+        }
+    }
+};
+
 /* Users */
 /* Products */
 /* Newsletters */
-/* Notifications */
 /* Home */
 /* Contacts */
