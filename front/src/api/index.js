44da4c7eb63db684c6d107160d57d0dcd0d0abd3
Samlogy <senanisammy@gmail.com>
2021-09-19 10:32:17 +0200

    add: (front) api calls users (load - create - edit - delete - filter - block)

--- front/src/api/index.js
+++ front/src/api/index.js
@@ -108,6 +108,132 @@ export const hide_notification = async(notificationId) => {
 };
 
 /* Users */
+export const load_users = async () => {
+    try {
+        const res = await request("get", `/users/getUsers`, {});
+        if (res.success) {
+            const { data, success, message } = res;
+            return {
+                success,
+                data,
+                message
+            }
+        }
+
+    } catch (err) {
+        console.log('Operation Error occured --> : ', err);
+        return {
+            success: false,
+            error: err.message
+        }
+    }
+};
+
+export const create_user = async (data) => {
+    try {
+        const res = await request("post", `/users/createUser`, {}, data);
+        if (res.success) {
+            const { data, success, message } = res;
+            return {
+                success,
+                data,
+                message
+            }
+        }
+
+    } catch (err) {
+        console.log('Operation Error occured --> : ', err);
+        return {
+            success: false,
+            error: err.message
+        }
+    }
+};
+
+export const edit_user = async (userId, data) => {
+    try {
+        const res = await request("put", `/users/editUser/${userId}`, {}, data);
+        if (res.success) {
+            const { data, success, message } = res;
+            return {
+                success,
+                data,
+                message
+            }
+        }
+
+    } catch (err) {
+        console.log('Operation Error occured --> : ', err);
+        return {
+            success: false,
+            error: err.message
+        }
+    }
+};
+
+export const delete_user = async (userId) => {
+    try {
+        const res = await request("delete", `/users/deleteUser/${userId}`, {});
+        if (res.success) {
+            const { data, success, message } = res;
+            return {
+                success,
+                data,
+                message
+            }
+        }
+
+    } catch (err) {
+        console.log('Operation Error occured --> : ', err);
+        return {
+            success: false,
+            error: err.message
+        }
+    }
+};
+
+export const filter_user = async (value, filter_type) => {
+    try {
+        const res = await request("get", `/users/filterUsers?queryString=${value}&filterType=${filter_type}`, {});
+        if (res.success) {
+            const { data, success, message } = res;
+            return {
+                success,
+                data,
+                message
+            }
+        }
+
+    } catch (err) {
+        console.log('Operation Error occured --> : ', err);
+        return {
+            success: false,
+            error: err.message
+        }
+    }
+};
+
+export const block_user = async (userId, data) => {
+    try {
+        const res = await request("put", `/users/blockUser/${userId}`, {}, data);
+        if (res.success) {
+            const { data, success, message } = res;
+            return {
+                success,
+                data,
+                message
+            }
+        }
+
+    } catch (err) {
+        console.log('Operation Error occured --> : ', err);
+        return {
+            success: false,
+            error: err.message
+        }
+    }
+};
+
 /* Products */
 /* Newsletters */
 /* Home */
