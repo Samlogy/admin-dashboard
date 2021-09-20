0eeacfd6e5ad6981e837885d1f8b303b00294b67
Samlogy <senanisammy@gmail.com>
2021-09-20 10:20:30 +0200

    refactor: (front) Features Component

--- front/src/Pages/Products/Features.jsx
+++ front/src/Pages/Products/Features.jsx
@@ -1,13 +1,13 @@
 import React, { useState } from "react";
-import { Input, 
+import { Input, Flex,
     Box, Text, IconButton,
     Tag, TagLabel, TagCloseButton,
     } from "@chakra-ui/react"
 import { MdAddToPhotos } from "react-icons/md"
 
-const Features = (props) => {
-  const { value, onChange } = props;
+import { View } from "../../Components";
 
+const Features = ({ value, onChange }) => {
   const [feature, setFeature] = useState("");
   const [features, setFeatures] = useState((value && value.length > 0) ? [...value] : []);
 
@@ -31,34 +31,36 @@ const Features = (props) => {
         return prevState.filter((item, idx) => idx !== featureIndex)
       })
   };
-  const displayFeatures = () => {
-    return features.map((item, idx) => (
-      <Box className="feature-item" key={idx}>
-        <Tag size="md" key={idx} borderRadius="full" variant="solid" mr=".2rem" mb=".25rem"
-            textColor="blue.700" bg="white" border="1px" borderColor="blue">
-          <TagLabel> {item} </TagLabel>
-          <TagCloseButton onClick={() => removeFeature(idx)}  />
-        </Tag>
-      </Box>
-    ));
-  };
 
   return (
     <>
-      { features && features.length > 0 ? 
-        <Box display="flex" flexDirection="row" flexWrap="wrap" mb=".5rem">
-            { displayFeatures() }
-        </Box> : <Text mb=".5rem" fontStyle="italic" textAlign="left"> features list empty ! </Text>
-      }
-        <Box display="flex" flexDirection="row" mb="1rem">
+      <View if={features && features.length > 0} display="flex" flexDirection="row" flexWrap="wrap" mb=".5rem">
+        { features.map((item, idx) => (
+            <Flex flexDir="row" flexWrap="wrap" key={idx}>
+              <Tag size="md" key={idx} borderRadius="full" bg="white" border="1px solid" borderColor="blue" m=".25rem"
+                  color="blue.700" variant="solid">
+                <TagLabel> {item} </TagLabel>
+                <TagCloseButton onClick={() => removeFeature(idx)} />
+              </Tag>
+            </Flex>
+          )) }
+      </View>
+
+      <View if={!features || features.length === 0}>
+        <Text mb=".5rem" fontStyle="italic" textAlign="left"> Features list empty ! </Text>
+      </View>
+
+        <Flex mb="1rem">
           <Input type="text" placeholder="New Feature..."
               value={feature} onChange={(e) => setFeature(e.target.value)} />
 
           <IconButton colorScheme="blue" aria-label="add feature" ml=".2rem" icon={<MdAddToPhotos size="20" />} 
                       onClick={() => addFeature()} />
-        </Box>
+        </Flex>
     </>
   );
 };
+
+
 
 export default Features;
