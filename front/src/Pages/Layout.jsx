2336ec7b4503999e6440c80261f2bd3b51b271b6
Samlogy <senanisammy@gmail.com>
2021-09-22 08:50:51 +0200

    refactor: (front) basic changes

--- front/src/Pages/Layout.jsx
+++ front/src/Pages/Layout.jsx
@@ -10,7 +10,7 @@ const Layout = ({ isFixedNav, isVisible, children, ...rest}) => {
 
   return (
     <Flex flexDirection="column" minHeight="100%" w="100%" {...rest}>
-      { (isVisible && isVisible) &&
+      { isVisible &&
         <NavBar isFixedNav={isFixedNav} 
                 logo="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKJoJokT_Vv4P4I0xnp5AIum9cUf75992G4mgIwkZQW4IISttYo9SuYZQjhZm0XjcEX7o&usqp=CAU" 
                 avatar={authStore && authStore.userData.avatar} 
@@ -21,7 +21,7 @@ const Layout = ({ isFixedNav, isVisible, children, ...rest}) => {
       <Flex alignItems="center" justifyContent="top" flexDirection="column"
             py={isFixedNav ? { md: "4rem" } : "0"}>
 
-        {children && children}
+        {children}
 
       </Flex>
     </Flex>
